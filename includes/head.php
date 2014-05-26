48db1f438afd0de7b9362f0f44df155f539f8201
fredchien <fred_chien@yahoo.com.br>
2014-05-26 16:09:16 -0300

    Footer
    Footers

---
+++ includes/head.php
@@ -0,0 +1,34 @@
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+<meta name="distribution" content="global" />
+<meta name="rating" content="general" />
+<meta name="robots" content="ALL" />
+<meta name="robots" content="index,follow"/>
+<meta name="language" content="pt-br" />
+<meta name="doc-rights" content="Public" />
+<meta name="classification" content="Servicos" />
+<meta name="Author" content="If Web Studio" />
+<meta http-equiv="Content-Language" content="pt-br" />
+
+<link rel="apple-touch-icon" sizes="114x114" href="img/favicon.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="img/favicon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="img/favicon.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon.png">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon.png">
+<link rel="shortcut icon" href="img/favicon.ico" />
+<link rel="icon" type="image/vnd.microsoft.icon" href="img/favicon.ico" />
+<link rel="icon" type="image/png" href="img/favicon.png" />
+<link rel="canonical" href="http://<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>" /> 
+<link type="text/css" rel="stylesheet" href="css/style.css" media="screen" />
+<!-- <link type="text/css" rel="stylesheet" href="css/lightbox.css" media="screen" />
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/lightbox-2.6.min.js"></script>
+<script type="text/javascript" src="js/responsiveslides.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/infobox/1.1.5/src/infobox.js"></script>
+<script type="text/javascript" src="js/custom.js"></script> -->
+
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<![endif]-->
---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+
+<meta name="keywords" content="" /> 
+<meta name="description" content="" /> 
+
+<?php include 'includes/head.php';?>
+</head>
+<body>
+<header>
+	<div class="center">
+		<img src="img/logo.png" alt="" title="">
+	</div>
+	<ul class="indiomas">
+		<li><a href=""><img src="img/br_flag.png" alt="" title=""></a>
+		<li><a href=""><img src="img/es_flag.png" alt="" title=""></a>
+		<li><a href=""><img src="img/us_flag.png" alt="" title=""></a>
+	</ul>
+	<div class="clear"></div>
+	
+	<nav id="menu">
+		<div class="center">
+			<ul>
+				<li><a href="#"><img src="img/home_1.png" alt="" title="" ></a>
+				<li><span>|</span>
+				<li><a href="#">fretamentos curitiba</a>
+				<li><span>|</span>
+				<li><a href="#">quem somos</a>
+				<li><span>|</span>
+				<li><a href="#">diferenciais</a>
+				<li><span>|</span>
+				<li><a href="#">orçamento</a>
+				<li><span>|</span>
+				<li><a href="#">contato</a>
+			</ul>
+		</div>
+		<div class="clear"></div>	
+	</nav>
+</header>
+<div class="clear"></div>
+<section id="banner">
+	<ul class="rslides">
+	  <li><img src="img/topo-home.jpg" alt="" title=""></li>
+	</ul>
+	<div class="clear"></div>
+</section>
+<section>
+	<div class="center">
+		<article class="tabela">
+			<a href="#">PEÇA UM ORÇAMENTO</a>
+		</article>
+	</div>
+</section>
+
+<section>
+	<div class="center">
+		<article>
+			<h1>TRANSPORTE EXECUTIVO CURITIBA FRETAMENTO</h1>
+			<p>A <b>ALT Transporte Executivo e Empresa de Fretamento</b> tem como proposta a prestação diferenciada de serviços
+			de locomoção coletiva como: <b>transporte executivo, fretamento e turismo, transporte de van,
+			aluguel de van, transporte de funcionários.</b></p>
+			<p>O veículo estendido para <b>transporte executivo</b> é o preferido de grandes empresas, pois apresenta detalhes
+			cuidadosos como, por exemplo, bancos customizados superconfortáveis, teto alto, wifi a bordo, cortinas, luz
+			de LED, etc.</p>
+			<p>A ALT <b>Transporte Executivo</b> e <b>Fretamento</b> preza pelo conforto, educação e discrição nos serviços.
+			Solicite um orçamento para sua demanda de <b><u>transporte executivo</u></b> e <b><u>fretamento em Curitiba</u></b> e região.</p>
+		</article>
+	</div>
+</section>
+	
+<section>
+	<div class="center">
+		<article>
+			<h1>PADRÃO DOS VEÍCULOS TRANSPORTE EXECUTIVO</h1>
+			
+			<ul>
+				<li><span title="wifi 4G" class="tooltip"><img src="img/h_01.png" alt="" src=""></span>
+				<li><span title="mp3 USB + phone" class="tooltip"><img src="img/h_02.png" alt="" src=""></span>
+				<li><span title="tv" class="tooltip"><img src="img/h_03.png" alt="" src=""></span>
+				<li><span title="motorista bilíngue" class="tooltip"><img src="img/h_04.png" alt="" src=""></span>
+				<li><span title="ar condicionado" class="tooltip"><img src="img/h_05.png" alt="" src=""></span>
+				<li><span title="emissão reduzida de poluentes" class="tooltip"><img src="img/h_06.png" alt="" src=""></span>
+				<li><span title="Bebidas" class="tooltip"><img src="img/h_07.png" alt="" src=""></span>
+				<li><span title="Crédito, Débito, Faturamento" class="tooltip"><img src="img/h_08.png" alt="" src=""></span>
+				<li><span title="Iluminação LED" class="tooltip"><img src="img/h_09.png" alt="" src=""></span>
+				<li><span title="disponibilidade 24h" class="tooltip"><img src="img/h_10.png" alt="" src=""></span>
+			</ul>
+		</article>
+	</div>
+</section>
+
+<footer>
+<section>
+	<div class="center">
+		<nav>
+			<ul>
+				<li><a href="#">HOME</a>
+			</ul>
+			
+			<ul>
+				<li><a href="#">FRETAMENTOS CURITIBA</a>
+			</ul>
+		</nav>
+	</div>
+</section>		
+</footer>
+<div class="center">
+	<address>Av. Presidente Kennedy, 1491 sala 104 • Curitiba - PR • 41 3117 9477 • alt@alt.tur.br</address>
+	<address>América Latina Turismo 2014 • © Todos os direitos reservados</address>
+</div>
+</body>
+</html>
